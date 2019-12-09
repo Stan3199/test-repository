3d052210b7802fe1790b66a28f9f50305b35c8b2
Stan3199 <Piyush.nautiyal85@gmail.com>
2019-12-09 10:42:37 +0530

    Updates

---
+++ precache-manifest.e4a13115d3ced8a16b19f5e1b376eea6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8a74775896388f06e18b9ad9bf8c95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22393de7302ee4ef8d28",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8d488ba309e761561245",
+    "url": "/static/js/2.3d736199.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3d736199.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22393de7302ee4ef8d28",
+    "url": "/static/js/main.f50dfd7f.chunk.js"
+  },
+  {
+    "revision": "062fdeebd2a943d67b91",
+    "url": "/static/js/runtime-main.d1db91c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
